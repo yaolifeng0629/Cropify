9e729a661ccc8a85af76c5d30adc7f05f6c04d52
Immerse <y15829485647@163.com>
2025-07-27 21:58:11 +0800

    feat: remove Header and Footer components from AppLayout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import React from 'react';
-import { Header } from './Header';
-import { Footer } from './Footer';
 
 interface AppLayoutProps {
     children: React.ReactNode;
@@ -10,9 +8,7 @@ interface AppLayoutProps {
 export const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
     return (
         <div className="min-h-screen bg-white">
-            <Header />
             <main className="max-w-7xl mx-auto px-6 py-8">{children}</main>
-            <Footer />
         </div>
     );
 };
